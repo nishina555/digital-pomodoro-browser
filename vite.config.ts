8baa354fcbd3e2c704c3949ef2f1b4424bd12c44
nishina555 <n.toshi.1.014@gmail.com>
2023-11-06 20:36:53 +0900

    add emotion

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // Emotionが提供するcssプロパティ等を反映するために必要となる
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
 })
