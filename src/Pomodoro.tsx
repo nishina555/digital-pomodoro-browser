0efa1a43d61382938714ac99453e14cbe99614d5
nishina555 <n.toshi.1.014@gmail.com>
2023-11-06 21:40:53 +0900

    themeを追加

--- src/Pomodoro.tsx
+++ src/Pomodoro.tsx
@@ -15,6 +15,13 @@ const TimerState = {
 
 type TimerState = "waiting" | "work" | "break";
 
+const Theme = {
+  Dark: "dark",
+  Light: "light",
+} as const;
+
+type ThemeType = "dark" | "light";
+
 type PomodoroReducerPayload = {
   workTime: number;
   breakTime: number;
@@ -53,8 +60,24 @@ const getUrlParamsWithDefaults = (locationSearch: string) => {
   const workTime = parseInt(urlParams.get("work") || "25") * 60;
   const breakTime = parseInt(urlParams.get("break") || "5") * 60;
   const startFrom = parseInt(urlParams.get("startFrom") || "0") * 60;
+  const theme = getTheme(urlParams.get("theme"));
+  const opacity = getOpacity(urlParams.get("opacity"));
+  return { workTime, breakTime, startFrom, theme, opacity };
+};
 
-  return { workTime, breakTime, startFrom };
+const getTheme = (theme: string | null) => {
+  // デフォルトのテーマはlight。
+  // リクエストパラメータがlightかdarkの場合はその値を、それ以外の場合はlightを使う。
+  if (theme === Theme.Dark) {
+    return Theme.Dark;
+  }
+  return Theme.Light;
+};
+
+const getOpacity = (opacity: string | null) => {
+  // opacityのデフォルトは1（不透過）。
+  // opacityは0から1の間の値を取る。リクエストパラメータが0未満の場合は0、1より大きい場合は1にする。
+  return Math.max(0, Math.min(1, parseFloat(opacity || "1")));
 };
 
 const pomodoroReducer = (
@@ -78,7 +101,7 @@ const pomodoroReducer = (
 
 export const Pomodoro = () => {
   const locationSearch = window.location.search;
-  const { workTime, breakTime, startFrom } =
+  const { workTime, breakTime, startFrom, theme, opacity } =
     getUrlParamsWithDefaults(locationSearch);
 
   const [pomodoroState, dispatch] = useReducer(pomodoroReducer, {
@@ -96,11 +119,11 @@ export const Pomodoro = () => {
   );
 
   return (
-    <div css={AppStyle}>
+    <div css={AppStyle(opacity, theme)}>
       <h1
         data-testid={"display-text"}
         id="display-text"
-        css={TextStyle(pomodoroState.timerState)}
+        css={TextStyle(pomodoroState.timerState, theme)}
       >
         {getDisplayText(pomodoroState.timerState, minutes, seconds)}
       </h1>
@@ -108,25 +131,30 @@ export const Pomodoro = () => {
   );
 };
 
-const AppStyle = css({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "100vh",
-});
+const AppStyle = (opacity: number, theme: ThemeType) =>
+  css({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor:
+      theme === Theme.Light
+        ? `rgba(255, 255, 255, ${opacity})`
+        : `rgba(0, 0, 0, ${opacity})`,
+  });
 
-const getColorByTimerState = (timerState: TimerState) => {
+const getColorByTimerState = (timerState: TimerState, theme: ThemeType) => {
   const colorMap = {
     [TimerState.Break]: "green",
     [TimerState.Work]: "red",
-    [TimerState.Waiting]: "initial",
+    [TimerState.Waiting]: theme === Theme.Light ? "initial" : "white",
   };
 
   return colorMap[timerState];
 };
 
-const TextStyle = (timerState: TimerState) =>
+const TextStyle = (timerState: TimerState, theme: ThemeType) =>
   css({
-    color: getColorByTimerState(timerState),
+    color: getColorByTimerState(timerState, theme),
   });
