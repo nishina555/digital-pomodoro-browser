cf6ec5fdd6efa7e4d9efa9baa7179cbe8addcf2a
nishina555 <n.toshi.1.014@gmail.com>
2023-11-09 20:48:38 +0900

    github pagesの設定

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "/digital-pomodoro-browser/" : './',
+  build: {
+    // `root` からの相対パスで指定する
+    outDir: './docs',
+  },
   plugins: [
     // Emotionが提供するcssプロパティ等を反映するために必要となる
     react({
