64689278ce3a7b9338507e92e731d4b0cdab1b12
nishina555 <n.toshi.1.014@gmail.com>
2023-11-09 21:37:41 +0900

    remove homepage

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  // base: process.env.GITHUB_PAGES ? "/digital-pomodoro-browser/" : './',
+  // base: './',
+  base: process.env.GITHUB_PAGES ? "/digital-pomodoro-browser/" : './',
   build: {
     // `root` からの相対パスで指定する
     outDir: './docs',
