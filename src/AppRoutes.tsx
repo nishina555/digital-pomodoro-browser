550233086ec8b0d9e7951c6d3cedc67bc18d8a89
nishina555 <n.toshi.1.014@gmail.com>
2023-11-09 22:25:37 +0900

    dev

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { PomodoroContainer } from "./PomodoroContainer";
 
 export const AppRoutes = () => {
   return (
+    // For GitHub Pages
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/pomodoro" element={<PomodoroContainer />} />
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: './',
+  // For GitHub Pages
   base: "/digital-pomodoro-browser/",
-  // base: "/digital-pomodoro-browser/",
+
   build: {
     // `root` からの相対パスで指定する
     outDir: './docs',
